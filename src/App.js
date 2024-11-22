8b31b2b0a180c66a33b1f1b5448b4b73766a6d6e
dayofmolp <molpofdays@gmail.com>
2024-11-22 13:12:32 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to My React Website!</h1>
+            <p>This is a simple React app to demonstrate functionality.</p>
+        </div>
+    );
+}
+
+export default App;
